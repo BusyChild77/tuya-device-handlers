2397d0fd32802fead5d2afdcec6c2ab2a6109f1b
busychild77 <nbe.acc.001@gmail.com>
2026-08-17 11:24:28 +0200

    Added Quirk for tjvnxyobs3upidjo

---
+++ src/tuya_device_handlers/devices/qn/tjvnxyobs3upidjo.py
@@ -0,0 +1,15 @@
+from tuya_device_handlers import TUYA_QUIRKS_REGISTRY
+from tuya_device_handlers.builder import DeviceQuirk
+from tuya_device_handlers.const import DPMode
+
+(
+    DeviceQuirk()
+    .applies_to(product_id="tjvnxyobs3upidjo")
+    .add_dpid_enum(
+        dpid=4,
+        dpcode="mode",
+        dpmode=DPMode.READ | DPMode.WRITE,
+        enum_range=["eco", "off"],
+    )
+    .register(TUYA_QUIRKS_REGISTRY)
+)
